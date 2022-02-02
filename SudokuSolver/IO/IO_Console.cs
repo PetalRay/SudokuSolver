334c5c2e5dd8781243e0fa9ec229ef5d8a947ac6
Maya Waxman <mayawaxman1@gmail.com>
2022-02-02 21:16:33 +0200

    Input Output process
    Input output management

---
+++ SudokuSolver/IO/IO_Console.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.IO
+{
+    class IO_Console : IO.IRead, IO.IWrite
+    {
+        public string Read()
+        {
+            Console.Write("Enter board string: ");
+            return Console.ReadLine();
+        }
+
+        public void Write(String outputString)
+        {
+            Console.WriteLine(outputString);
+        }
+    }
+}
---
+++ SudokuSolver/IO/IO_TxtFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuSolver.IO
+{
+    class IO_txtFile : IO.IRead, IO.IWrite
+    {
+
+        public string Read()
+        {
+            string path = Get_path();
+            string values = System.IO.File.ReadAllText(path);
+            return values;
+        }
+
+        // Function receives path of file from file explorer
+        public string Get_path()
+        {
+            OpenFileDialog pick_file = new OpenFileDialog();
+            if (pick_file.ShowDialog() == DialogResult.OK)
+                return pick_file.FileName;
+            return "No Path";
+        }
+
+
+        public void Write(String outputString)
+        {
+
+        }
+
+    }
+}
